a1326237b7349a7fb57778156b107ca56126b2d4
RaysonYT <nikolomoec@gmail.com>
2022-11-13 22:11:54 +0200

    Organise Files Add Content Model

---
+++ LearningApp/ViewModels/ContentModel.swift
@@ -0,0 +1,14 @@
+//
+//  ContentModel.swift
+//  LearningApp
+//
+//  Created by Nikita Kolomoec on 13.11.2022.
+//
+
+import Foundation
+
+class ContentModel: ObservableObject {
+    
+    
+    
+}
---
+++ LearningApp/Views/HomeView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  LearningApp
+//
+//  Created by Nikita Kolomoec on 13.11.2022.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    
+    @EnvironmentObject var model: ContentModel
+    
+    var body: some View {
+        
+        Text("")
+        
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        HomeView()
+    }
+}
