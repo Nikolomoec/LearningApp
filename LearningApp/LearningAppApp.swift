15c395efa517f41ecb2f8d21e0ccc05795b1326a
RaysonYT <nikolomoec@gmail.com>
2022-11-13 22:05:05 +0200

    Initial Commit

---
+++ LearningApp/LearningAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningAppApp.swift
+//  LearningApp
+//
+//  Created by Nikita Kolomoec on 13.11.2022.
+//
+
+import SwiftUI
+
+@main
+struct LearningAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
